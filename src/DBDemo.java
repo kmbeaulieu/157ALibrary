ffd463ea53da07b8ab214d836810ca78c8ec40c3
Krystle <krystleweinrich@yahoo.com>
2016-11-27 17:18:44 -0800

    added drop table if, added correct attributes for user/loan. NO need for trying to drop each table now!

--- src/DBDemo.java
+++ src/DBDemo.java
@@ -118,59 +118,69 @@ public class DBDemo {
 
 		// Create user tables
 		try {
+			//drop table if exists so we dont run into problems being able to create it.
+			this.executeUpdate(conn, "DROP TABLE IF EXISTS USER");
+			System.out.println("drop table if exists user executed");
+			
 		    String createString =
 			        "CREATE TABLE " + this.userTableName + " ( " +
-			        "ID INTEGER NOT NULL, " +
-			        "NAME varchar(40) NOT NULL, " +
-			        "STREET varchar(40) NOT NULL, " +
-			        "CITY varchar(20) NOT NULL, " +
-			        "STATE char(2) NOT NULL, " +
-			        "ZIP char(5), " +
-			        "PRIMARY KEY (ID))";
+			        "UID INTEGER NOT NULL, " +
+			        "NAME VARCHAR(30), " +
+			        "ISEMPLOYEE TINYINT DEFAULT 0, " +
+			        "BORROWED INT DEFAULT 0, " +
+			        "BIRTHDATE DATE DEFAULT '0000-00-00', " +
+			        "FEES DOUBLE DEFAULT 0.0, " +
+			        "UPDATEDON TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,"+ 
+			        "CHECK(ISEMPLOYEE<2), "+
+			        "PRIMARY KEY (UID))";
 			this.executeUpdate(conn, createString);
 			System.out.println("Created a " + this.userTableName + " table");
-	    } catch (SQLException e) {
+			//do the increment for user
+			this.executeUpdate(conn, "ALTER TABLE USER AUTO_INCREMENT = 1");
+			System.out.println("altered user table to increment from 1.");
+		} catch (SQLException e) {
 			System.out.println("ERROR: Could not create the "+ this.userTableName+ " table");
 			e.printStackTrace();
 			return;
 		}
-		
-		// Drop the user table
-		try {
-		    String dropString = "DROP TABLE " + this.userTableName;
-			this.executeUpdate(conn, dropString);
-			System.out.println("Dropped the " +this.userTableName+" table");
-	    } catch (SQLException e) {
-			System.out.println("ERROR: Could not drop the "+this.userTableName+" table");
-			e.printStackTrace();
-			return;
-		}
+//		
+//		// Drop the user table KEEP THIS FOR FUTURE REFERENCE BUT THIS IS CURRENTLY NOT NEEDED IF THERE IS THE DROP IF... STATEMENT PER TABLE
+//		try {
+//		    String dropString = "DROP TABLE " + this.userTableName;
+//			this.executeUpdate(conn, dropString);
+//			System.out.println("Dropped the " +this.userTableName+" table");
+//	    } catch (SQLException e) {
+//			System.out.println("ERROR: Could not drop the "+this.userTableName+" table");
+//			e.printStackTrace();
+//			return;
+//		}
 		
 		// Create loan tables
 				try {
+					//drop table if exists so we dont run into problems being able to create it.
+					this.executeUpdate(conn, "DROP TABLE IF EXISTS LOAN");
+					System.out.println("drop table if exists loan executed");
+					
 				    String createString =
 					        "CREATE TABLE " + this.loanTableName + " ( " +
-					        "ID INTEGER NOT NULL, " +
-					        "NAME varchar(40) NOT NULL, " +
-					        "STREET varchar(40) NOT NULL, " +
-					        "CITY varchar(20) NOT NULL, " +
-					        "STATE char(2) NOT NULL, " +
-					        "ZIP char(5), " +
-					        "PRIMARY KEY (ID))";
+					        "LOANID INTEGER AUTO_INCREMENT, " +
+					        "UID INT, " +
+					        "BOOKID INT, " +
+					        "CHECKOUTDATE DATE DEFAULT '0000-00-00', " +
+					        "DUEDATE DATE DEFAULT '0000-00-00', " +
+					        "OVERDUE TINYINT DEFAULT 0, " +
+					        "CHECK(OVERDUE<2), " +
+					        "PRIMARY KEY (LOANID), " +
+					        "FOREIGN KEY (UID) references USER (UID))";
+				    //TODO make a foreign key for bookid after book is made
 					this.executeUpdate(conn, createString);
 					System.out.println("Created a " + this.loanTableName + " table");
+					
+					this.executeUpdate(conn, "ALTER table LOAN AUTO_INCREMENT = 1001");
+					System.out.println("LOAN table now increments starting at 1001.");
+
 			    } catch (SQLException e) {
 					System.out.println("ERROR: Could not create the "+ this.loanTableName+ " table");
-					e.printStackTrace();
-					return;
-				}
-				// Drop the loan table
-				try {
-				    String dropString = "DROP TABLE " + this.loanTableName;
-					this.executeUpdate(conn, dropString);
-					System.out.println("Dropped the " +this.loanTableName+" table");
-			    } catch (SQLException e) {
-					System.out.println("ERROR: Could not drop the "+this.loanTableName+" table");
 					e.printStackTrace();
 					return;
 				}
