324c32b2b463ffc292704c266ee755a835486e34
Krystle <krystleweinrich@yahoo.com>
2016-12-04 21:42:54 -0800

    pay now works. Launch from MainMenupage for real data manipulation

--- src/UserPaymentPage.java
+++ src/UserPaymentPage.java
@@ -5,10 +5,12 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.sql.SQLException;
 import java.util.Date;
 
 import javax.swing.JTextField;
@@ -50,6 +52,7 @@ public class UserPaymentPage extends JFrame {
 		contentPane.setLayout(null);
 		//fill in passed in info, set up database connection
 		final User user = usr;
+		final DatabaseManager dbm = new DatabaseManager();
 		//create content
 		JLabel lblUserPayment = new JLabel("User Payment");
 		lblUserPayment.setFont(new Font("Tahoma", Font.BOLD, 15));
@@ -64,24 +67,46 @@ public class UserPaymentPage extends JFrame {
 		lblPaymentAmount.setBounds(82, 121, 100, 14);
 		contentPane.add(lblPaymentAmount);
 		
-		JLabel feesLabel = new JLabel("$ " + user.getFees());
+		final JLabel feesLabel = new JLabel("$ " + user.getFees());
 		feesLabel.setBounds(178, 76, 86, 14);
 		contentPane.add(feesLabel);
 		
+		final JFormattedTextField paymentAmountTextfield = new JFormattedTextField();
+		paymentAmountTextfield.setBounds(185, 118, 86, 20);
+		contentPane.add(paymentAmountTextfield);
+		
 		JButton payNowButton = new JButton("Pay Now");
+		payNowButton.addMouseListener(new MouseAdapter(){
+			@Override
+			public void mouseClicked(MouseEvent arg0){
+				
+				try {
+					double newFees = dbm.payFees(user.getUid(), Double.valueOf(paymentAmountTextfield.getText()));
+					paymentAmountTextfield.setValue(newFees);
+					feesLabel.setText("$ " + newFees);
+					
+				} catch (NumberFormatException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		});
 		payNowButton.setBounds(175, 181, 89, 23);
 		contentPane.add(payNowButton);
 		
-		JFormattedTextField paymentAmountTextfield = new JFormattedTextField();
-		paymentAmountTextfield.setBounds(185, 118, 86, 20);
-		contentPane.add(paymentAmountTextfield);
+		
 		
 		JButton button = new JButton("<");
 		button.addMouseListener(new MouseAdapter() {
 		@Override
 		public void mouseClicked(MouseEvent arg0) {
 			dispose();
-			new UserProfilePage(user);
+			//go back with the updated info
+			new UserProfilePage(dbm.selectUser(user.getName(), user.getUid()));
 		}
 	});
 		button.setBounds(10, 11, 46, 23);
